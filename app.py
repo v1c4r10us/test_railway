43bf6eee4800e102fc7254b9218321b2a8ebe9e1
v1c4r10us <v1c4r10us.29@gmail.com>
2023-04-14 17:27:23 -0500

    return a df

--- app.py
+++ app.py
@@ -6,5 +6,24 @@ app=FastAPI()
 
 @app.get('/')
 def get_df():
-    return json.loads(elt.get_df())
+    return {'server': 'Railway running...'}
 
+@app.get('/gmd/{platform}')
+def get_max_duration(year:int, platform:str, duration_type:str):
+    return {'movie':elt.get_max_duration(year, platforms[platform], duration_type)}
+
+@app.get('/gcp/{platform}')
+def get_count_platform(platform:str):
+    return {'total_movies':elt.get_count_platform(platforms[platform])}
+
+@app.get('/ga/{platform}')
+def get_actor(platform:str, year:int):
+    return {'actor': elt.get_actor(platforms[platform], year)}
+
+@app.get('/ppc')
+def prod_per_country(tipo:str, pais:str, anio:int):
+    return elt.prod_per_country(tipo, pais, anio)
+
+@app.get('/gc')
+def get_contents(rating:str):
+    return elt.get_contents(rating)
--- interface.py
+++ interface.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 #Streaming Datasets
 df_a=pd.read_csv('https://drive.google.com/uc?id=1WJfCKOAp2UhWfXEBSXHtiIe4ShBw48pB')
-#df_d=pd.read_csv('https://drive.google.com/uc?id=187comTc0dz1aqGLSXQ5gLzY70RALoRpJ')
-#df_h=pd.read_csv('https://drive.google.com/uc?id=1z6v7Sx4wBkjEBSKz5cp2lMID2EYYIQY4')
-#df_n=pd.read_csv('https://drive.google.com/uc?id=1eArA5pc0zGgn1w2ujBiKkf1hSEf1_Fjk')
+df_d=pd.read_csv('https://drive.google.com/uc?id=187comTc0dz1aqGLSXQ5gLzY70RALoRpJ')
+df_h=pd.read_csv('https://drive.google.com/uc?id=1z6v7Sx4wBkjEBSKz5cp2lMID2EYYIQY4')
+df_n=pd.read_csv('https://drive.google.com/uc?id=1eArA5pc0zGgn1w2ujBiKkf1hSEf1_Fjk')
 
 #Rating datasets
 #df1=pd.read_csv('https://drive.google.com/uc?id=1RpQwvS0W8AqIsV8bP51iJzDwLSch_o1d')
@@ -18,5 +18,68 @@ df_a=pd.read_csv('https://drive.google.com/uc?id=1WJfCKOAp2UhWfXEBSXHtiIe4ShBw48
 #df8=pd.read_csv('https://drive.google.com/uc?id=1-kfhN2jPDGZVXOnswgUQa2031m3RUUiD')
 #df_rate=[df1, df2, df3, df4, df5, df6, df7, df8]
 
-def get_df():
-    return df_a.to_json()
+#Functions
+def transform(input_dataframe, idx_char):
+    df=input_dataframe
+    df.insert(1,'id',idx_char+df[['show_id']]) #Generating Id
+    df['rating'].fillna('G', inplace=True) #Replacing NA of rating
+    df['date_added']=df.date_added.str.strip() ##Fixing netflix problem
+    df['date_added']=pd.to_datetime(df['date_added'], format='%B %d, %Y') #Formating dates
+    df[['duration_int','duration_type']]=df.duration.str.split(" ", expand=True) #Splitting duration
+    df['type']=df.type.str.lower() #Lowercase for text fields
+    df['title']=df.title.str.lower()
+    df['director']=df.director.str.lower()
+    df['cast']=df['cast'].astype('str') ##Fixing hulu problem
+    df['cast']=df.cast.str.lower()
+    df['country']=df.country.str.lower()
+    df['rating']=df.rating.str.lower()
+    df['listed_in']=df.listed_in.str.lower()
+    df['description']=df.description.str.lower()
+    df['duration_type']=df.duration_type.str.lower()
+    return df
+
+def get_max_duration(year, platform, duration_type):
+    df=platform
+    rows=df[(df['type']=='movie')&(df['release_year']==year)&(df['duration_type']==duration_type)].sort_values(by='duration_int')
+    if rows.shape[0]>0:
+        resp=rows.iloc[0]['title']
+    else:
+        resp=None
+    return resp
+
+def get_count_platform(platform):
+    df=platform
+    rows=df[df['type']=='movie']
+    return rows.shape[0]
+
+def get_actor(platform, year):
+    df=platform
+    df=df[df['release_year']==year]
+    actors=[y for x in df['cast'] for y in x.split(', ')]
+    actors_w_count={x: actors.count(x) for x in actors}
+    del actors_w_count['nan']
+    if len(actors_w_count)>0:
+        m=max(actors_w_count, key=actors_w_count.get)
+    else:
+        m=None
+    return m
+
+def prod_per_country(tipo,pais,anio):
+    rows_a=amazon[(amazon['type']==tipo) & (amazon['release_year']==anio) & (amazon['country']==pais)].shape[0]
+    rows_d=disney[(disney['type']==tipo) & (disney['release_year']==anio) & (disney['country']==pais)].shape[0]
+    rows_h=hulu[(hulu['type']==tipo) & (hulu['release_year']==anio) & (hulu['country']==pais)].shape[0]
+    rows_n=netflix[(netflix['type']==tipo) & (netflix['release_year']==anio) & (netflix['country']==pais)].shape[0]
+    return {'pais': pais, 'anio': anio, tipo:rows_a+rows_d+rows_h+rows_n}
+
+def get_contents(rating):
+    rows_a=amazon[amazon['rating']==rating].shape[0]
+    rows_d=disney[disney['rating']==rating].shape[0]
+    rows_h=hulu[hulu['rating']==rating].shape[0]
+    rows_n=netflix[netflix['rating']==rating].shape[0]
+    return {rating: rows_a+rows_d+rows_h+rows_n}
+
+#Transformed datasets
+amazon=transform(df_a, 'a')
+disney=transform(df_d, 'd')
+hulu=transform(df_h, 'h')
+netflix=transform(df_n, 'n')
